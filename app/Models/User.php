ce5ae27bfdd698a7dd8d17ba4ccb78eaa7ef8365
Salah Ashraf <salahashraf924@gmail.com>
2025-05-10 21:54:34 +0300

    fix: remove unused imports and clean up User model

--- app/Models/User.php
+++ app/Models/User.php
@@ -2,7 +2,6 @@
 
 namespace App\Models;
 
-// use Illuminate\Contracts\Auth\MustVerifyEmail;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Foundation\Auth\User as Authenticatable;
 use Illuminate\Notifications\Notifiable;
@@ -11,7 +10,6 @@ use Laravel\Sanctum\HasApiTokens;
 class User extends Authenticatable
 {
     /** @use HasFactory<\Database\Factories\UserFactory> */
-    use HasFactory, Notifiable;
 
     use HasApiTokens, HasFactory, Notifiable;
 
@@ -55,5 +53,4 @@ class User extends Authenticatable
     {
         return $this->hasMany(Reservation::class);
     }
-
 }
